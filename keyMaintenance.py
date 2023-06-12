ab2e686324dc2c7840d6286af29f78d2e5db1867
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-06-12 18:40:16 +0100

    Indentação

--- keyMaintenance.py
+++ keyMaintenance.py
@@ -39,31 +39,31 @@ def rotate_vertical(matrix, col, n):
 # Função para gerar as matrizes ZA, ZB, ZC e ZD (ou ZS) e a matriz Z
 def generate_matrices(M, K, use_zs=True):
 	"""Seja M1 uma sequência com os primeiros K bytes de M e M2 outra sequência com os K bytes
-    restantes de M (ou seja, M = M1+M2);"""
+	restantes de M (ou seja, M = M1+M2);"""
 	M1 = M[:K]
 	M2 = M[K:]
 
 	"""Criar uma matriz ZA de K x K bytes em que a primeira linha é igual a M1 e as restantes linhas
-    são iguais a rotate(M1,i), em que i é o índice da linha e a função rotate(M,n) retorna uma
-    sequência com o mesmo comprimento de M mas com os seus elementos rodados pela direita n
-    vezes; por exemplo, rotate(“012345”,1) = “501234”;"""
+	são iguais a rotate(M1,i), em que i é o índice da linha e a função rotate(M,n) retorna uma
+	sequência com o mesmo comprimento de M mas com os seus elementos rodados pela direita n
+	vezes; por exemplo, rotate(“012345”,1) = “501234”;"""
 	ZA = [rotate(M1, i) for i in range(K)]
 
 	"""Criar uma matriz ZB de K x K bytes em que a primeira coluna é igual a transpose(M2) e as
-    restantes colunas são iguais a transpose(rotate(M2,j)), em que j é o índice da coluna e a função
-    transpose(M) retorna uma sequência com o mesmo comprimento de M mas com os seus
-    elementos alinhados/organizados verticalmente, de cima para baixo, numa coluna, se M for uma
-    sequência com os elementos alinhados/organizados horizontalmente; se M já for uma sequência
-    com os elementos alinhados/organizados verticalmente a função retorna uma sequência com o
-    mesmo comprimento de M mas com os seus elementos alinhados/organizados horizontalmente,
-    da esquerda para a direita, numa linha"""
+	restantes colunas são iguais a transpose(rotate(M2,j)), em que j é o índice da coluna e a função
+	transpose(M) retorna uma sequência com o mesmo comprimento de M mas com os seus
+	elementos alinhados/organizados verticalmente, de cima para baixo, numa coluna, se M for uma
+	sequência com os elementos alinhados/organizados horizontalmente; se M já for uma sequência
+	com os elementos alinhados/organizados verticalmente a função retorna uma sequência com o
+	mesmo comprimento de M mas com os seus elementos alinhados/organizados horizontalmente,
+	da esquerda para a direita, numa linha"""
 	ZB = [transpose(rotate(M2, j))[0] for j in range(K)]
 	ZB = list(map(list, zip(*ZB)))
 
 	"""Criar uma matriz ZC de K x K bytes em que ZC[i,j] = random(ZA[i,j],0,255), em que a função
-    random(seed,min,max)1 retorna um número aleatório entre min e max usando a semente seed;
-    criar uma matriz ZD de K x K bytes em que ZD[i,j] = random(ZB[i,j],0,255); em alternativa pode
-    ser criada apenas uma matriz ZS de K x K bytes em que ZS[i,j] = random(S,0,255);"""
+	random(seed,min,max)1 retorna um número aleatório entre min e max usando a semente seed;
+	criar uma matriz ZD de K x K bytes em que ZD[i,j] = random(ZB[i,j],0,255); em alternativa pode
+	ser criada apenas uma matriz ZS de K x K bytes em que ZS[i,j] = random(S,0,255);"""
 	if use_zs:
 		ZS = [[random_with_seed(random.randint(0, 255), 0, 255) for _ in range(K)] for _ in range(K)]
 	else:
@@ -71,16 +71,16 @@ def generate_matrices(M, K, use_zs=True):
 		ZD = [[random_with_seed(ZB[i][j], 0, 255) for j in range(K)] for i in range(K)]
 
 	"""Calcular uma matriz Z de K x K bytes em que Z[i,j] = xor(ZA[i,j],ZB[i,j],ZC[i,j],ZD[i,j]) ou, em
-    alternativa, Z[i,j] = xor(ZA[i,j],ZB[i,j],ZS[i,j]), e em que xor(A,B,C,…) devolve um byte que é o
-    resultado de aplicar a função booleana xor (Exclusive OR) aos seus argumentos (bytes) duma
-    forma binária (bit a bit); a função xor pode ser substituída por uma outra função fm(a,b) que
-    devolve um valor a partir de dois argumentos de tal forma que o valor devolvido e os argumentos
-    têm valores possíveis no mesmo limite e todos os valores possivelmente devolvidos são
-    uniformemente distribuídos; a implementação desta de fm pode apresentar vantagens na
-    implementação em relação à função xor se for implementada à custa dum simples acesso a um
-    array predefinido e quando os valores possíveis dos argumentos são limitados (neste caso, a
-    função fm pode ser implementada como o acesso direto a um elemento dum array predefinido no
-    ficheiro F)."""
+	alternativa, Z[i,j] = xor(ZA[i,j],ZB[i,j],ZS[i,j]), e em que xor(A,B,C,…) devolve um byte que é o
+	resultado de aplicar a função booleana xor (Exclusive OR) aos seus argumentos (bytes) duma
+	forma binária (bit a bit); a função xor pode ser substituída por uma outra função fm(a,b) que
+	devolve um valor a partir de dois argumentos de tal forma que o valor devolvido e os argumentos
+	têm valores possíveis no mesmo limite e todos os valores possivelmente devolvidos são
+	uniformemente distribuídos; a implementação desta de fm pode apresentar vantagens na
+	implementação em relação à função xor se for implementada à custa dum simples acesso a um
+	array predefinido e quando os valores possíveis dos argumentos são limitados (neste caso, a
+	função fm pode ser implementada como o acesso direto a um elemento dum array predefinido no
+	ficheiro F)."""
 	if use_zs:
 		Z = [[ZS[i][j] ^ ZA[i][j] ^ ZB[i][j] for j in range(K)] for i in range(K)]
 	else:
